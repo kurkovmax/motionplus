631a2c04a6b9b9b60bf92626a7f72ccf5b8c5494
Medet Kurkov <kurkov.lil.ink@gmail.com>
2025-07-20 17:42:26 +0500

    Обновил сайт

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <>
       <App />
       <Toaster
-        position="top-center"
+        position="center"
         toastOptions={{
           duration: 4000,
           style: {
