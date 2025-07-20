2e9763c79e6ba5be52535f20565aff3c8d829f34
Medet Kurkov <kurkov.lil.ink@gmail.com>
2025-07-20 17:41:15 +0500

    Обновил сайт

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,32 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// 👇 добавляем Toaster
+// Уведомления
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
       <App />
-      <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#111',
+            color: '#fff',
+            fontSize: '14px',
+            border: '1px solid #333',
+            padding: '12px 20px',
+          },
+          success: {
+            icon: '✅',
+          },
+          error: {
+            icon: '❌',
+          },
+        }}
+      />
     </>
   </StrictMode>
 );
