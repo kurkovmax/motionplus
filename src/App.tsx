8c27ea6d7cba11d627b8040354077c6cf9becc35
Medet Kurkov <kurkov.lil.ink@gmail.com>
2025-07-22 22:55:37 +0500

    Обновил сайт

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
 import RunningLine from './components/RunningLine';
-//import Workflow from './components/Workflow';
+// import Workflow from './components/Workflow';
 import Services from './components/Services';
 import BottomContact from './components/BottomContact';
-//import Contacts from './components/Contacts';
+// import Contacts from './components/Contacts';
 import Footer from './components/Footer';
 
 function App() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const id = hash.replace('#', '');
+      setTimeout(() => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 300); // Подождать, пока блоки точно отрисуются
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-black">
       <Navbar />
