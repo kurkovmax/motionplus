6f265303979d3cb822c998493635453df7ce0a93
Medet Kurkov <kurkov.lil.ink@gmail.com>
2025-07-22 23:59:26 +0500

    Обновил сайт

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero: React.FC = () => {
     {/* Заголовок */}
     <div className="absolute top-[28%] max-w-[90%] sm:max-w-[600px]">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight">
-        Мы креативное <br /> агентство <br /> медиапроизводства
+        Мы креативное <br /> агентство <br /> медиаконтента
       </h1>
     </div>
 
